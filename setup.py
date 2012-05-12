adc817dd0bbae4c658853c02bada880c2fb76dfe
Ariel Núñez <ingenieroariel@gmail.com>
2012-05-12 15:23:15 -0400

    Bump to 0.3.2

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def read(*rnames):
 
 setup(
     name="gisdata",
-    version="0.3.2",
+    version="0.3.3",
     author="ingenieroariel",
     author_email="ingenieroariel@gmail.com",
     description="Sample data for GIS packages",
