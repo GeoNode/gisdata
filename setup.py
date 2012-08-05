6425fdd62f756838262d7739d67898ee974870f9
Ariel Núñez <ingenieroariel@gmail.com>
2012-08-05 17:48:49 -0400

    Created a function to find all data, to avoid having to manually specify

--- setup.py
+++ setup.py
@@ -4,6 +4,17 @@ from distutils.core import setup
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
+def all_dirs(location):
+    output = []
+    for path, dirs, files in os.walk(location):
+        for d in dirs:
+            item = os.path.join(path, d, '*')
+            # Get rid of the first 'gisdata'
+            valid_dir = os.sep.join(item.split(os.sep)[1:])
+            output.append(valid_dir)
+    return output
+
+
 setup(
     name="gisdata",
     version="0.4.2",
@@ -21,10 +32,6 @@ setup(
     keywords="gis",
     url='https://github.com/GeoNode/gisdata',
     packages=['gisdata',],
-    package_data={'gisdata': ['data/good/vector/*',
-                              'data/good/raster/*',
-                              'data/bad/*',
-                              'data/good/time/*',
-                              ]},
+    package_data={'gisdata': all_dirs('gisdata'),},
     zip_safe=False,
 )
